992908b9663702555ab687a3a447bab34baa9699
chelseaphoebe <chelsea.arantza@student.umn.ac.id>
2025-07-25 01:36:00 +0700

    fix pages

--- GoLoop/src/pages/landingPage/Home.jsx
+++ GoLoop/src/pages/landingPage/Home.jsx
@@ -1,16 +1,14 @@
+// src/pages/landingPage/Home.jsx
+
 import React from "react";
+import { Link as ScrollLink } from "react-scroll";
 import gambar1 from "../../assets/img/bg-home.png";
-import gambar3 from "../../assets/img/bg-wilayah-3.svg";
-import img1 from "../../assets/img/goloop1.png"; // ganti sesuai nama file kamu
-import img2 from "../../assets/img/goloop2.png";
-import img3 from "../../assets/img/goloop3.png";
 
 function Home() {
   return (
     <div className="font-quicksand">
       {/* Section Hero */}
-
-      <div className="relative w-full h-[600px] overflow-hidden">
+      <div className="relative w-full h-[600px] overflow-hidden" id="hero">
         <img
           src={gambar1}
           alt="Background"
@@ -32,85 +30,16 @@ function Home() {
               membangun ekosistem daur ulang yang kolaboratif dan transparan
               untuk mengurangi dampak sampah terhadap lingkungan.
             </p>
-            <button className="bg-white text-green-900 font-semibold px-6 py-3 rounded-full shadow hover:bg-gray-200 transition">
-              Ikut berkontribusi
-            </button>
+            <ScrollLink to="wilayah" smooth={true} duration={500}>
+              <button className="bg-white text-green-900 font-semibold px-6 py-3 rounded-full shadow hover:bg-gray-200 transition">
+                Ikut berkontribusi
+              </button>
+            </ScrollLink>
           </div>
         </div>
       </div>
 
-      {/* Section Wilayah dengan Sampah Terbanyak */}
-      {/* Pembungkus ini menggunakan "items-center" untuk memosisikan semua elemen di dalamnya (termasuk gambar) ke tengah secara horizontal. */}
-      <div className="w-full pt-12 h-[600px] px-6 bg-white flex flex-col items-center text-center">
-        <h2 className="text-4xl font-bold mb-10" style={{ color: "#2C441E" }}>
-          Wilayah dengan Sampah Terbanyak
-        </h2>
-
-        <img
-          src={gambar3}
-          alt="Wilayah Sampah"
-          className="w-auto pl-60 h-auto"
-        />
       </div>
-      <div className="text-center mb-12">
-        <h2 className="text-4xl font-bold pt-8 text-[#334222] mb-4">What We Do?</h2>
-        <p className="text-[#334222] text-md font-semibold">
-          GoLoop jadi wadah untuk siapa pun yang ingin bantu bersihin sampah di
-          jalanan dan ruang publik. <br />
-          Kami mengumpulkan relawan & komunitas untuk aksi bersih-bersih, dan
-          pastikan sampah dibuang ke TPA resmi.
-        </p>
-      </div>
-
-      <div className="grid grid-cols-1 px-24 md:grid-cols-3 gap-8">
-        {/* Card 1 */}
-        <div className="rounded-[40px] overflow-hidden shadow-lg bg-[#3E532D] text-white">
-          <img
-            src={img1}
-            alt="GoLoop participation"
-            className="w-full h-64 object-cover"
-          />
-          <div className="p-6">
-            <h3 className="text-xl font-semibold mb-2">GoLoop participation</h3>
-            <p className="text-sm">
-              ikut dalam membersihkan lingkungan tempat kita hidup
-            </p>
-          </div>
-        </div>
-
-        {/* Card 2 */}
-        <div className="rounded-[40px] overflow-hidden shadow-lg bg-[#3E532D] text-white">
-          <img
-            src={img2}
-            alt="Waste Sorting"
-            className="w-full h-64 object-cover"
-          />
-          <div className="p-6">
-            <h3 className="text-xl font-semibold mb-2">Waste Sorting</h3>
-            <p className="text-sm">
-              belajar memilah sampah organik, anorganik, dan B3 agar pengolahan
-              sampah lebih efektif dan ramah lingkungan
-            </p>
-          </div>
-        </div>
-
-        {/* Card 3 */}
-        <div className="rounded-[40px] overflow-hidden shadow-lg bg-[#3E532D] text-white">
-          <img
-            src={img3}
-            alt="Awareness Increase"
-            className="w-full h-64 object-cover"
-          />
-          <div className="p-6">
-            <h3 className="text-xl font-semibold mb-2">Awareness Increase</h3>
-            <p className="text-sm">
-              tingkatkan kesadaranmu terhadap sekitar jadikan tempat yang layak
-              untuk kita tinggal
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
   );
 }
 
